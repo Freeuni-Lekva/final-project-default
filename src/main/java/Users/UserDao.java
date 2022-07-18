cfd54fbb048e94fbd90f7ae6be99d006cce03057
nikagobe <91562629+nikagobe@users.noreply.github.com>
2022-07-18 18:30:40 +0400

    Uploaded User Services
    Uploaded Tested User class, User Data acess class, which handles database acess of users and friends tables, also uploaded UserServices class which handles every service associated with User objects.

---
+++ src/main/java/Users/UserDao.java
@@ -0,0 +1,155 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class UserDao implements IUserDao {
+    Connection conn;
+
+    public UserDao(String base , String user , String password) throws SQLException {
+        conn = DriverManager.getConnection(base , user , password);
+    }
+
+
+    @Override
+    public void getUsers(HashSet<User> resultSet) throws SQLException {
+        Statement stm = conn.createStatement();
+        ResultSet res = stm.executeQuery("select * from users");
+        while(res.next()){
+            User newUser = new User(res.getInt(1) , res.getString(2) , res.getBoolean(4));
+            resultSet.add(newUser);
+        }
+        stm.close();
+    }
+
+    @Override
+    public User addUser(String userName , String password_hash) throws SQLException {
+        Statement stm = conn.createStatement();
+        String query = "insert into users(username , password_hash , is_admin) values ('"+ userName + "','" + password_hash + "', false)";
+        stm.execute(query);
+        return getUser(userName);
+    }
+
+    @Override
+    public boolean updateUser(User user , String newpassword_hash) throws SQLException {
+        Statement stm = conn.createStatement();
+        int changed = stm.executeUpdate("update users set username = '" + user.getUsername() + "', password_hash = '" + newpassword_hash + "',is_admin = " + user.isAdmin() +
+                " where id = " + user.getId());
+        if(changed < 1) return false;
+        return true;
+    }
+
+    public boolean updateUser(User user) throws SQLException {
+        Statement stm = conn.createStatement();
+        int changed = stm.executeUpdate("update users set username = '" + user.getUsername() + "' ,is_admin = " + user.isAdmin() +
+                " where id = " + user.getId());
+        if(changed < 1) return false;
+        return true;
+    }
+
+    @Override
+    public User getUser(String userName) throws SQLException {
+        Statement stm = conn.createStatement();
+        ResultSet res = stm.executeQuery("select * from users where username = '" + userName + "'");
+        if(!res.next()) return null;
+        User newUser = new User(res.getInt(1) , res.getString(2) , res.getBoolean(4));
+        return newUser;
+    }
+
+    @Override
+    public User getUser(int id) throws SQLException {
+        Statement stm = conn.createStatement();
+        ResultSet res = stm.executeQuery("select * from users where id = " + id);
+        if(!res.next()) return null;
+        User newUser = new User(res.getInt(1) , res.getString(2) , res.getBoolean(4));
+        return newUser;
+    }
+
+    @Override
+    public boolean addFriend(User sender , User receiver) throws SQLException {
+        Statement stm = conn.createStatement();
+        boolean changed = stm.execute("insert into friends(user_Id , friend_Id , accepted) values ('" + sender.getId() + "'," + receiver.getId() + "," + false +")");
+        return changed;
+    }
+
+    @Override
+    public boolean acceptFriend(User sender , User receiver) throws SQLException {
+        Statement stm = conn.createStatement();
+        int changed = stm.executeUpdate("update friends set accepted = true where user_Id = " + sender.getId() + " and friend_Id = " + receiver.getId());
+        if(changed < 1) return false;
+        return true;
+    }
+
+    @Override
+    public boolean tryLogin(String username, String password_hash) throws SQLException {
+        Statement stm = conn.createStatement();
+        ResultSet res = stm.executeQuery("select password_hash from users where username = '" + username + "'");
+        if(!res.next()) return false;
+        String hash = res.getString(1);
+        if(hash.equals(password_hash)) return true;
+        return false;
+    }
+
+    @Override
+    public List<User> searchByUsername(String userName) throws SQLException {
+        List<User> resultList = new ArrayList<>();
+        Statement stm = conn.createStatement();
+        ResultSet res = stm.executeQuery("select * from users where username like '%" + userName + "%'");
+        while(res.next()){
+            User newUser = new User(res.getInt(1) , res.getString(2) , res.getBoolean(4));
+            resultList.add(newUser);
+        }
+        return resultList;
+    }
+
+    @Override
+    public List<User> getFriends(User user) throws SQLException {
+        List<User> resultList = new ArrayList<>();
+        Statement stm = conn.createStatement();
+        HashSet<Integer> ids = new HashSet<>();
+        ResultSet res = stm.executeQuery("select * from friends where user_id = " + user.getId() +  " and accepted = true");
+        while(res.next()){
+            ids.add(res.getInt(3));
+        }
+        res = stm.executeQuery("select * from friends where friend_id = " + user.getId() +  " and accepted = true");
+        while(res.next()){
+            ids.add(res.getInt(2));
+        }
+
+        for(Integer i : ids){
+            resultList.add(getUser(i));
+        }
+        return resultList;
+    }
+
+    @Override
+    public List<User> getSentRequests(User user) throws SQLException {
+        List<User> resultList = new ArrayList<>();
+        Statement stm = conn.createStatement();
+        HashSet<Integer> ids = new HashSet<>();
+        ResultSet res = stm.executeQuery("select * from friends where user_id = " + user.getId() +  " and accepted = false");
+        while(res.next()){
+            ids.add(res.getInt(3));
+        }
+        for(Integer i : ids){
+            resultList.add(getUser(i));
+        }
+        return resultList;
+    }
+
+    @Override
+    public List<User> getReceivedRequests(User user) throws SQLException {
+        List<User> resultList = new ArrayList<>();
+        Statement stm = conn.createStatement();
+        HashSet<Integer> ids = new HashSet<>();
+        ResultSet res = stm.executeQuery("select * from friends where friend_id = " + user.getId() +  " and accepted = false");
+        while(res.next()){
+            ids.add(res.getInt(2));
+        }
+
+        for(Integer i : ids){
+            resultList.add(getUser(i));
+        }
+        return resultList;
+    }
+}
---
+++ src/main/java/Users/UserService.java
@@ -0,0 +1,140 @@
+import javax.jws.soap.SOAPBinding;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserService {
+    private IUserDao userDao;
+
+    MessageDigest md;
+    private static final String PASSWORD_PATTERN_STRING =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+
+    private static final Pattern pasword_pattern = Pattern.compile(PASSWORD_PATTERN_STRING);
+
+    private static final String USERNAME_PATTERN_STRING =
+            "^[a-zA-Z0-9._-]{3,}$";
+
+    private static final Pattern username_pattern = Pattern.compile(USERNAME_PATTERN_STRING);
+
+    public UserService() throws SQLException, NoSuchAlgorithmException {
+        userDao = new UserDao("jdbc:mysql://localhost:3306/quiz" , "root" , "password");
+        md = MessageDigest.getInstance("SHA");
+    }
+
+    public boolean makeUserAdmin(User user) throws SQLException {
+        user.setAdmin(true);
+        return userDao.updateUser(user);
+    }
+
+    public boolean changeUsername(User user , String newUsername) throws SQLException {
+        if(userDao.getUser(newUsername) != null) return false;
+        user.setUsername(newUsername);
+        return userDao.updateUser(user);
+    }
+
+    public boolean changePassword(User user , String newPassword) throws SQLException {
+        Matcher passMatcher = pasword_pattern.matcher(newPassword);
+        if(!passMatcher.matches()) return false;
+
+        md.update(newPassword.getBytes());
+        byte[] hash = md.digest();
+        String hashString = hexToString(hash);
+
+        return userDao.updateUser(user , hashString);
+    }
+
+    public boolean login(String username, String password) throws SQLException {
+        md.update(password.getBytes());
+        byte[] hash = md.digest();
+        String hashString = hexToString(hash);
+        return userDao.tryLogin(username , hashString);
+    }
+
+    public User getUser(String username) throws SQLException {
+        return userDao.getUser(username);
+    }
+
+    public User getUser(int id) throws SQLException {
+        return userDao.getUser(id);
+    }
+
+    public User addUser(String username , String password) throws SQLException {
+        if(!match(username , password)) return null;
+
+        md.update(password.getBytes());
+        byte[] hash = md.digest();
+        String hashString = hexToString(hash);
+
+        return userDao.addUser(username , hashString);
+    }
+
+    private boolean match(String username , String password) {
+        Matcher passMatcher = pasword_pattern.matcher(password);
+        if(!passMatcher.matches()) return false;
+        Matcher usMatcher = username_pattern.matcher(username);
+        if(!usMatcher.matches()) return false;
+        return true;
+    }
+
+    public boolean addFriend(User sender , User receiver) throws SQLException {
+
+        if(userDao.getFriends(sender).stream().anyMatch(c -> c.getUsername().equals(receiver.getUsername()))) return false;
+
+        if(userDao.getSentRequests(sender).stream().anyMatch(c -> c.getUsername().equals(receiver.getUsername()))) return false;
+
+        if(userDao.getReceivedRequests(sender).stream().anyMatch(c -> c.getUsername().equals(receiver.getUsername()))){
+            userDao.acceptFriend(receiver , sender);
+            return true;
+        }
+
+        userDao.addFriend(sender , receiver);
+        return true;
+
+    }
+
+    public boolean acceptFriend(User sender , User receiver) throws SQLException {
+        return userDao.acceptFriend(sender , receiver);
+    }
+
+    public List<User> searchByUsername(String username) throws SQLException{
+        return userDao.searchByUsername(username);
+    }
+
+
+    /*
+	 Given a byte[] array, produces a hex String,
+	 such as "234a6f". with 2 chars for each byte in the array.
+	 (provided code)
+	*/
+    private static String hexToString(byte[] bytes) {
+        StringBuffer buff = new StringBuffer();
+        for (int i=0; i<bytes.length; i++) {
+            int val = bytes[i];
+            val = val & 0xff;  // remove higher bits, sign
+            if (val<16) buff.append('0'); // leading 0
+            buff.append(Integer.toString(val, 16));
+        }
+        return buff.toString();
+    }
+
+    /*
+     Given a string of hex byte values such as "24a26f", creates
+     a byte[] array of those values, one byte value -128..127
+     for each 2 chars.
+     (provided code)
+    */
+
+    private static byte[] hexToArray(String hex) {
+        byte[] result = new byte[hex.length()/2];
+        for (int i=0; i<hex.length(); i+=2) {
+            result[i/2] = (byte) Integer.parseInt(hex.substring(i, i+2), 16);
+        }
+        return result;
+    }
+}
