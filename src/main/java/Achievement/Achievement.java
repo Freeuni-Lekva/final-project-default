20b6ae8aa8d51bcef6720ae3c83ce842ebd6744c
kkara20 <91481287+kkara20@users.noreply.github.com>
2022-07-28 18:17:50 +0400

    Commiting Achievement
    Updated old achievement classes and also added AchievementService

---
+++ src/main/java/Achievement/Achievement.java
@@ -0,0 +1,40 @@
+public class Achievement implements IAchievement {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Achievement(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{id = ");
+        builder.append(this.id);
+        builder.append("{, name = ");
+        builder.append(this.name);
+        builder.append(", description = ");
+        builder.append(this.description);
+        builder.append("}");
+        return builder.toString();
+    }
+}
---
+++ src/main/java/Achievement/AchievementHistory.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+
+public class AchievementHistory implements IAchievement {
+
+    private int id;
+    private Achievement achievement;
+    private Date date;
+
+    public AchievementHistory(int id,  Achievement achievement, Date date) {
+        this.id = id;
+        this.achievement = achievement;
+        this.date = date;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return achievement.getName();
+    }
+
+    @Override
+    public String getDescription() {
+        return achievement.getDescription();
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{date = ");
+        builder.append(this.date.toString());
+        builder.append(", name = ");
+        builder.append(getName());
+        builder.append(", description = ");
+        builder.append(getDescription());
+        builder.append("}");
+        return builder.toString();
+    }
+}
