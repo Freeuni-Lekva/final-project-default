87da00aba7b827c6eb73e101f3d7ba8ecea4abb5
gzang20 <91743469+zangura77@users.noreply.github.com>
2022-07-29 15:59:35 +0400

    ფუნქციების signature-ები შეიცვალა
    addAnswer, addQuestion და addQuiz ფუნქციები void იყო. შევცვალე ისე რომ addAnswer - Answer-ს აბრუნებს, addQuestion - Question-ს და addQuiz - Quiz-ს.

--- src/main/java/Quizs/IQuestionDao.java
+++ src/main/java/Quizs/IQuestionDao.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public interface IQuestionDao {
     ArrayList<Question> getQuestions() throws SQLException;
     Question getQuestion(int id) throws SQLException;
-    void addQuestion(int quizId, String type, String description) throws SQLException;
+    Question addQuestion(int quizId, String type, String description) throws SQLException;
     boolean removeQuestion(Question question) throws SQLException;
     boolean removeQuestion(int questionId) throws SQLException;
     boolean setQuestionDescription(Question question, String description) throws SQLException;
@@ -12,7 +12,8 @@ public interface IQuestionDao {
     boolean updateQuestion(Question question) throws SQLException;
     ArrayList<Answer> getAnswers(Question question) throws SQLException;
     ArrayList<Answer> getAnswers(int questionId) throws SQLException;
-    void addAnswer(Question question, String description, boolean isCorrect) throws SQLException;
+    Answer addAnswer(Question question, String description, boolean isCorrect) throws SQLException;
+    Answer addAnswer(int questionId, String description, boolean isCorrect) throws SQLException;
     boolean removeAnswer(Answer answer) throws SQLException;
     boolean removeAnswer(int answerId) throws SQLException;
     boolean removeQuestionAnswers(Question question) throws SQLException;
