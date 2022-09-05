5d1d7016876858343d5596a480121856111f4352
gzang20 <91743469+zangura77@users.noreply.github.com>
2022-09-05 19:16:50 +0400

    Reuploading DAO changes

---
+++ IQuizDao.java
@@ -0,0 +1,36 @@
+package Quizs;
+import Users.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface IQuizDao {
+    ArrayList<Quiz> getQuizzes() throws SQLException;
+    ArrayList<Quiz> getQuizzes(User user) throws SQLException;
+    ArrayList<Quiz> getQuizzes(int userId) throws SQLException;
+    ArrayList<Quiz> getRecentQuizzes(int limit) throws SQLException;
+    ArrayList<Quiz> getRecentQuizzes(User user, int limit) throws SQLException;
+    ArrayList<Quiz> getRecentQuizzes(int userId, int limit) throws SQLException;
+    Quiz getQuiz(int id) throws SQLException;
+    Quiz addQuiz(String title, String description, int creatorId, int quizTime,boolean isRandom,boolean isOnePage,boolean immediateCorrection,boolean canBePracticed) throws SQLException;
+    boolean removeQuiz(Quiz quiz) throws SQLException;
+    boolean setQuizTitle(Quiz quiz, String title) throws SQLException;
+    boolean setQuizTitle(int quizId, String title) throws SQLException;
+    boolean setQuizRandomness(Quiz quiz, boolean isRandom) throws SQLException;
+    boolean setQuizRandomness(int quizId, boolean isRandom) throws SQLException;
+    boolean setQuizToOnePage(Quiz quiz, boolean isOnePage) throws SQLException;
+    boolean setQuizToOnePage(int quizId, boolean isOnePage) throws SQLException;
+    boolean setQuizImmediateCorrection(Quiz quiz, boolean isImmediateCorrection) throws SQLException;
+    boolean setQuizImmediateCorrection(int quizId, boolean isImmediateCorrection) throws SQLException;
+    boolean setQuizCanBePracticed(Quiz quiz, boolean canBePracticed) throws SQLException;
+    boolean setQuizCanBePracticed(int quizId, boolean canBePracticed) throws SQLException;
+    boolean setQuizDescription(Quiz quiz, String description) throws SQLException;
+    boolean setQuizDescription(int quizId, String description) throws SQLException;
+    boolean setQuizTime(Quiz quiz, int quizTime) throws SQLException;
+    boolean setQuizTime(int quizId, int quizTime) throws SQLException;
+    boolean updateQuiz(Quiz quiz) throws SQLException;
+    ArrayList<Question> getQuestions(Quiz quiz) throws SQLException;
+    ArrayList<Question> getQuestions(int quizId) throws SQLException;
+    boolean removeQuizQuestions(Quiz quiz) throws SQLException;
+    boolean removeQuizQuestions(int quizId) throws SQLException;
+}
---
+++ TestHelper.java
@@ -0,0 +1,64 @@
+package Quizs;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestHelper {
+    private static final String CREATE_USERS = "create table users(Id serial primary key,username varchar(50) unique," +
+            "password_hash varchar(200),is_admin boolean);";
+    private static final String CREATE_QUIZZES = "create table quizes(Id serial primary key,Title varchar(200)," +
+            "description varchar(1500),creator_id bigint unsigned,foreign key (creator_id) references users(id),quizTime bigint, " +
+            "isRandom boolean, isOnePage boolean, immediateCorrection boolean, canBePracticed boolean);";
+    private static final String CREATE_QUESTIONS = "create table questions(Id serial primary key,quiz_id bigint unsigned," +
+            "Question_type varchar(30),Question_Description varchar(2000),foreign key (quiz_id) references QUIZES(id));";
+    private static final String CREATE_ANSWERS = "create table answers(Id serial primary key,Question_id bigint unsigned," +
+            "answer_Description varchar(1000),is_correct boolean,foreign key (Question_id) references Questions(id));";
+    private static final String CREATE_HISTORY = "create table history(Id serial primary key,user_Id bigint unsigned," +
+            "quiz_id bigint unsigned,score integer,start_time date,end_time date,foreign key (user_id) references users(id)," +
+            "foreign key (quiz_id) references quizes(id));";
+    private static final String CREATE_FRIENDS = "create table friends(Id serial primary key,user_Id bigint unsigned," +
+            "friend_Id bigint unsigned,accepted boolean,foreign key (user_id) references users(id)," +
+            "foreign key (friend_Id) references users(id));";
+    private static final String CREATE_MAILS = "create table mails(Id serial primary key,from_Id bigint unsigned,to_Id bigint unsigned," +
+            "message_type varchar(5),message_title varchar(200),message_content varchar(1000),send_date date," +
+            "foreign key (from_Id) references users(id),foreign key (to_Id) references users(id));";
+    private static final String CREATE_ACHIEVEMENTS = "create table achievments(Id serial primary key,name varchar(100)," +
+            "description varchar(500));";
+    private static final String CREATE_ACHIEVEMENTS_HISTORY = "create table achievment_history(Id serial primary key," +
+            "user_Id bigint unsigned,achievment_Id bigint unsigned,acquired_date date,foreign key (user_Id) references users(id)," +
+            "foreign key (achievment_Id) references achievments(id));";
+
+    public static void deleteAndCreateDatabase(DatabaseConnection db) throws SQLException {
+        Statement st = db.getConnection().createStatement();
+        dropTables(st);
+        createTables(st);
+    }
+
+    private static void dropTables(Statement st) throws SQLException {
+        dropTable(st, "achievment_history");
+        dropTable(st, "achievments");
+        dropTable(st, "MAILS");
+        dropTable(st, "FRIENDS");
+        dropTable(st, "HISTORY");
+        dropTable(st, "ANSWERS");
+        dropTable(st, "QUESTIONS");
+        dropTable(st, "QUIZES");
+        dropTable(st, "USERS");
+    }
+
+    private static void dropTable(Statement st, String tableName) throws SQLException {
+        st.execute("DROP TABLE IF EXISTS " + tableName + ";");
+    }
+
+    private static void createTables(Statement st) throws SQLException {
+        st.execute(CREATE_USERS);
+        st.execute(CREATE_QUIZZES);
+        st.execute(CREATE_QUESTIONS);
+        st.execute(CREATE_ANSWERS);
+        st.execute(CREATE_HISTORY);
+        st.execute(CREATE_FRIENDS);
+        st.execute(CREATE_MAILS);
+        st.execute(CREATE_ACHIEVEMENTS);
+        st.execute(CREATE_ACHIEVEMENTS_HISTORY);
+    }
+}
