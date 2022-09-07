662959ccb54fa0ff8388bce3022a6927b5c531b2
gzang20 <91743469+zangura77@users.noreply.github.com>
2022-09-07 19:52:01 +0400

    Updated MailDao

--- src/main/java/Mails/IMailDao.java
+++ src/main/java/Mails/IMailDao.java
@@ -10,5 +10,6 @@ public interface IMailDao {
      void deleteMail(int id) throws SQLException;
      Mail getMail(int id) throws SQLException ;
      List<Mail> getUsersIncomingMails(int user_id) throws SQLException;
+     List<Mail> getUsersRecentIncomingMails(int userId) throws SQLException;
      List<Mail> getUsersOutgoingMails(int user_id) throws SQLException;
 }
